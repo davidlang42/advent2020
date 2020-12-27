a4001c76695df3c18d8cda17cb11d89574e4bf95
David Lang <davidlang42@gmail.com>
2020-12-28 01:14:07 +1100

    day9 complete

--- day9/part2/src/main.rs
+++ day9/part2/src/main.rs
@@ -6,43 +6,33 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() == 3 {
         let filename = &args[1];
-        let preamble_size: &usize = &args[2].parse().expect("Preamble must be an integer");
+        let target_sum: &usize = &args[2].parse().expect("Target Sum must be an integer");
         let text = fs::read_to_string(&filename)
             .expect(&format!("Error reading from {}", filename));
-        let data: Vec<usize> = text.split("\r\n").map(|line| line.parse()
+        let numbers: Vec<usize> = text.split("\r\n").map(|line| line.parse()
             .expect(&format!("Error parsing number: {}",line))).collect();
-        let result = process(&mut data.iter(), preamble_size).unwrap();
-        println!("Result: {}", result);
+        let mut data = numbers.iter();
+        let result = process(&mut data, target_sum);
+        let min = result.iter().min().unwrap();
+        let max = result.iter().max().unwrap();
+        println!("Result: {}", **min + **max);
     } else {
-        println!("Please provide 2 arguments: Filename, Preamble Size");
+        println!("Please provide 2 arguments: Filename, Target Sum");
     }
 }
 
-fn process(data: &mut Iter<usize>, preamble_size: &usize) -> Result<usize,String> {
-    let mut preamble: Vec<&usize> = Vec::new();
-    // fill preamble
-    for _i in 0..*preamble_size {
-        preamble.push(data.next().unwrap());
-    }
-    // check data
-    while let Some(value) = data.next() {
-        if !check_sum(&preamble, value) {
-            return Ok(value.clone()); // found mismatched value
-        }
-        preamble.remove(0);
-        preamble.push(value);
-    }
-    // failed if no errors found
-    Err("No mismatched data found.".to_string())
-}
-
-fn check_sum(list: &Vec<&usize>, target_value: &usize) -> bool {
-    for (index1, value1) in list.iter().enumerate() {
-        for value2 in list[(index1+1)..].iter() {
-            if **value1 != **value2 && **value1 + **value2 == *target_value {
-                return true; // 2 values in list sum to target
-            }
+fn process<'a>(data: &'a mut Iter<usize>, target_sum: &usize) -> Vec<&'a usize> {
+    let mut selection: Vec<&usize> = Vec::new();
+    let mut sum: usize = 0;
+    while !(sum == *target_sum && selection.len() > 1) {
+        if sum > *target_sum {
+            let remove_value = selection.remove(0);
+            sum -= remove_value;
+        } else {
+            let add_value = data.next().unwrap();
+            sum += add_value;
+            selection.push(add_value);
         }
     }
-    return false; // no sum found
+    selection
 }
