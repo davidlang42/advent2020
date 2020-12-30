f9411bc73755fd8da0ba3aab9c8432795dba9129
David Lang <davidlang42@gmail.com>
2020-12-30 11:16:10 +1100

    part2 works for small input

--- day10/part2/src/main.rs
+++ day10/part2/src/main.rs
@@ -1,47 +1,43 @@
+use std::collections::HashSet;
 use std::env;
 use std::fs;
 
-struct JoltageDifferences {
-    diff1: usize,
-    diff2: usize,
-    diff3: usize
-}
-
 fn main() {
     let args: Vec<String> = env::args().collect();
-    if args.len() == 2 {
+    if args.len() == 3 {
         let filename = &args[1];
+        let target: usize = args[2].parse().unwrap();
         let text = fs::read_to_string(&filename)
             .expect(&format!("Error reading from {}", filename));
-        let mut numbers: Vec<usize> = text.split("\r\n").map(|line| line.parse()
+        let numbers: HashSet<usize> = text.split("\r\n").map(|line| line.parse()
             .expect(&format!("Error parsing number: {}",line))).collect();
-        let result: JoltageDifferences = process(&mut numbers).unwrap();
-        println!("Diffs: {},{},{}", result.diff1, result.diff2, result.diff3);
-        println!("Result: {}", result.diff1 * result.diff3);
+        //println!("Numbers: {:?}", numbers);
+        let result = count_combinations(&numbers, 0, target);
+        println!("Result: {}", result);
     } else {
-        println!("Please provide 1 argument: Filename");
+        println!("Please provide 2 arguments: Filename, Device Joltage");
     }
 }
 
-fn process(list: &mut Vec<usize>) -> Result<JoltageDifferences, String> {
-    list.sort();
-    let mut joltages = list.iter();
-    let mut last_joltage: &usize = &0;
-    let mut result = JoltageDifferences {
-        diff1: 0,
-        diff2: 0,
-        diff3: 0
-    };
-    while let Some(next_joltage) = joltages.next() {
-        match *next_joltage - *last_joltage {
-            1 => result.diff1 += 1,
-            2 => result.diff2 += 1,
-            3 => result.diff3 += 1,
-            0 => return Err("You have 2 adapters with the same joltage".to_string()),
-            _ => return Err("You have adapters with a joltage difference greater than 3".to_string())
-        }
-        last_joltage = next_joltage;
+fn count_combinations(adapters: &HashSet<usize>, from: usize, to: usize) -> usize {
+    let mut count: usize = 0;
+    //println!("Finding combinations from {} to {}", from, to);
+    if adapters.contains(&(from+1)) {
+        //println!("Adapters contains {}", from+1);
+        count += count_combinations(adapters, from+1, to);
     }
-    result.diff3 += 1; // always final diff of 3 between highest adapter and your device
-    Ok(result)
+    if adapters.contains(&(from+2)) {
+        //println!("Adapters contains {}", from+2);
+        count += count_combinations(adapters, from+2, to);
+    }
+    if adapters.contains(&(from+3)) {
+        //println!("Adapters contains {}", from+3);
+        count += count_combinations(adapters, from+3, to);
+    }
+    if from+3 == to {
+        //println!("Final adapter from {} to {}", from, to);
+        count += 1;
+    }
+    //println!("Resulting combinations from {} to {}: {}", from, to, count);
+    return count;
 }
