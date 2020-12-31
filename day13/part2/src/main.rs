cbbf31cdf5ad0bb25009def3c4293a5a1f56c794
David Lang <davidlang42@gmail.com>
2020-12-31 15:42:08 +1100

    add starting point argument

--- day13/part2/src/main.rs
+++ day13/part2/src/main.rs
@@ -5,21 +5,25 @@ const NEW_LINE: &str = "\r\n";
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    if args.len() == 2 {
+    if args.len() == 3 {
         let filename = &args[1];
+        let start: usize = args[2].parse().unwrap();
         let text = fs::read_to_string(&filename)
             .expect(&format!("Error reading from {}", filename));
         let lines: Vec<&str> = text.split(NEW_LINE).collect();
-        let earliest_time: usize = lines[0].parse().expect("First line should be the earliest timestamp");
+        //let earliest_time: usize = lines[0].parse().expect("First line should be the earliest timestamp");
         let values: Vec<&str> = lines[1].split(",").collect();
         let possible_buses: Vec<Option<usize>> = values.iter().map(|s| s.parse().ok()).collect();
-        let mut timestamp = earliest_time;
+        let mut timestamp = start;
         while !verify(&possible_buses, &timestamp) {
             timestamp += 1;
+            if timestamp % 100000000 == 0 {
+                println!("{}",timestamp);
+            }
         }
         println!("Result: {}", timestamp);
     } else {
-        println!("Please provide 1 argument: Filename");
+        println!("Please provide 2 arguments: Filename, Starting point");
     }
 }
 
