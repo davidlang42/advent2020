55dc81d49435a5c5adfffde612da6ec13aabc1d8
David Lang <davidlang42@gmail.com>
2020-12-31 17:01:42 +1100

    add lcm (unused)

--- day13/part2/src/main.rs
+++ day13/part2/src/main.rs
@@ -1,5 +1,6 @@
 use std::env;
 use std::fs;
+//use num::integer::lcm;
 
 const NEW_LINE: &str = "\r\n";
 
@@ -14,6 +15,7 @@ fn main() {
         //let earliest_time: usize = lines[0].parse().expect("First line should be the earliest timestamp");
         let values: Vec<&str> = lines[1].split(",").collect();
         let possible_buses: Vec<Option<usize>> = values.iter().map(|s| s.parse().ok()).collect();
+        //let actual_buses: Vec<usize> = possible_buses.iter().filter(|o| o.is_some()).map(|o| o.unwrap()).collect();
         let mut timestamp = start;
         while !verify(&possible_buses, &timestamp) {
             timestamp += 1;
@@ -40,3 +42,14 @@ fn verify(list: &Vec<Option<usize>>, start: &usize) -> bool {
     }
     true
 }
+
+// fn lowest_common_multiple(list: &Vec<usize>) -> usize {
+//     let mut values = list.iter();
+//     let first: usize = *values.next().unwrap();
+//     let second: usize = *values.next().unwrap();
+//     let mut result: usize = lcm(first, second);
+//     while let Some(next) = values.next() {
+//         result = lcm(result, *next);
+//     }
+//     result
+// }
