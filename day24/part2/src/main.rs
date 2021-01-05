8b1e4e695e3a304aea9e6b1eb82a21c693d13008
David Lang <davidlang42@gmail.com>
2021-01-06 01:50:43 +1100

    part2

--- day24/part2/src/main.rs
+++ day24/part2/src/main.rs
@@ -2,7 +2,6 @@ use std::collections::HashMap;
 use std::env;
 use std::fs;
 use std::str::FromStr;
-use std::ops::AddAssign;
 
 const NEW_LINE: &str = "\r\n";
 
@@ -27,9 +26,20 @@ impl Direction {
             Direction::NorthEast => Point(1, -1)
         }
     }
+
+    fn options() -> Vec<Direction> {
+        vec![
+            Direction::East,
+            Direction::SouthEast,
+            Direction::SouthWest,
+            Direction::West,
+            Direction::NorthWest,
+            Direction::NorthEast
+        ]
+    }
 }
 
-#[derive(PartialEq)]
+#[derive(PartialEq, Clone, Copy)]
 enum Color {
     White,
     Black
@@ -42,9 +52,10 @@ struct Tile {
 #[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
 struct Point(isize, isize);
 
-impl AddAssign for Point {
-    fn add_assign(&mut self, other: Self) {
-        *self = Self(self.0 + other.0, self.1 + other.1)
+impl Point {
+    fn move_in_direction(&self, direction: &Direction) -> Self {
+        let offset = direction.offset();
+        Point(self.0 + offset.0, self.1 + offset.1)
     }
 }
 
@@ -83,6 +94,69 @@ impl Floor {
                 self.0.insert(point, new_tile);
             }
         }
+    }
+
+    fn get_tile_color(&self, point: &Point) -> Color {
+        match self.0.get(point) {
+            Some(existing_tile) => existing_tile.color,
+            None => Color::White
+        }
+    }
+
+    fn count_adjacent_black(&self, point: &Point) -> usize {
+        let mut count: usize = 0;
+        for direction in Direction::options() {
+            if self.get_tile_color(&point.move_in_direction(&direction)) == Color::Black {
+                count += 1;
+            }
+        }
+        count
+    }
+
+    fn bounds(&self) -> (Point, Point) {
+        let mut min = Point(0,0);
+        let mut max = Point(0,0);
+        for point in self.0.keys() {
+            if point.0 < min.0 {
+                min.0 = point.0;
+            }
+            if point.1 < min.1 {
+                min.1 = point.1;
+            }
+            if point.0 > max.0 {
+                max.0 = point.0;
+            }
+            if point.1 > max.1 {
+                max.1 = point.1
+            }
+        }
+        (min, max)
+    }
+
+    fn run_daily(&mut self) {
+        let mut new_tiles = HashMap::new();
+        let (min, max) = self.bounds();
+        for x in (min.0-1)..=(max.0+1) {
+            for y in (min.1-1)..=(max.1+1) {
+                let point = Point(x, y);
+                let adjacent_black = self.count_adjacent_black(&point);
+                let mut color = self.get_tile_color(&point);
+                match color {
+                    Color::Black => {
+                        if adjacent_black == 0 || adjacent_black > 2 {
+                            color = Color::White;
+                        }
+                    },
+                    Color::White => {
+                        if adjacent_black == 2 {
+                            color = Color::Black;
+                        }
+                    }
+                }
+                new_tiles.insert(point, Tile { color });
+            }
+        }
+        self.0 = new_tiles;
     }
 }
 
@@ -126,7 +200,7 @@ impl Directions {
     fn point(&self) -> Point {
         let mut point = Point(0, 0);
         for direction in self.0.iter() {
-            point += direction.offset();
+            point = point.move_in_direction(direction);
         }
         point
     }
@@ -134,19 +208,25 @@ impl Directions {
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    if args.len() == 2 {
+    if args.len() == 3 {
         let filename = &args[1];
         let text = fs::read_to_string(&filename)
             .expect(&format!("Error reading from {}", filename));
         let tiles_to_flip: Vec<Directions> = text.split(NEW_LINE).map(|s| s.parse()
             .expect(&format!("Error parsing Directions: {}", s))).collect();
+        let days: usize = args[2].parse().expect("Days should be a number");
+        // initial state
         let mut floor = Floor::new();
         for directions in tiles_to_flip {
             let point = directions.point();
             floor.flip_tile(point);
         }
-        println!("Result: {}", floor.count_black());
+        // day simulation
+        for _ in 0..days {
+            floor.run_daily();
+        }
+        println!("Day {}: {}", days, floor.count_black());
     } else {
-        println!("Please provide 1 argument: Filename");
+        println!("Please provide 2 arguments: Filename, Days");
     }
 }
